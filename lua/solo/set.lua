773031df55d65bf17af7aa347167d3b4c5e7572b
Florian 'Solo' Stertz <florian.stertz@gmail.com>
2025-05-15 20:17:02 +0200

    starting with light color

--- lua/solo/set.lua
+++ lua/solo/set.lua
@@ -1,1 +1,31 @@
+vim.o.guicursor = ""
+
+vim.o.nu = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+vim.o.smartindent = true
+
+vim.o.wrap = false
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undofile = true
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+vim.o.termguicolors = true
+
+vim.o.scrolloff = 8
+vim.o.signcolumn = "yes"
+
+vim.o.updatetime = 50
+
+vim.o.colorcolumn = "80"
 vim.o.background = "light"
